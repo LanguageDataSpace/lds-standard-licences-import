4e9c9cc66f3c41995bbe77b6bd9a3c3081307cca
Katerina Gkirtzou <katerina.gkirtzou@gmail.com>
2024-10-17 15:13:19 +0300

    Remove retrieval and transformation capabilities, keep only the import to LDS-EDC connector v0.7.0

--- README.md
+++ README.md
@@ -1,60 +1,7 @@
-A script that retrieves existing representations of standard licences expressed in ODRL v2.2 (https://www.w3.org/TR/odrl-model/),
-transforms them into the EDC policy representation, by merging info from multiple sources if required, and add them into an LDS-Connector.
+A script that adds EDC policy representations of standard licences into an LDS-Connector v0.7.0.
 
-## Retrieve
-Retrieve existing RDF representations of standard licences expressed in ODRL v2.2 (https://www.w3.org/TR/odrl-model/ from the following sources
-  - SPDX - https://github.com/spdx/license-list-data
-  - DALICC - https://api.dalicc.net/docs#/licenselibrary
-  - RDF License - https://rdflicense.linkeddata.es/
-and stores them in the folder `LicencesInit`
-
-Set which licences to retrieve from which source in the config file
-  - From SPDX set variable `spdx_id_rdfLicense`  
-  - From DALIICC set variable `dalicc_licences_id`
-  - From RDF License set variable `rdfLicense_licences_id`
-
-### Use ::
-` python main.py -r `
-
-## Tranform
-Transforms existing RDF representations of standard licences expressed in ODRL v2.2 stored in folder `LicencesInit` into the EDC policy
-representation. EDC policy representations are stored in folder `LicencesAdded`. 
-Transformation involves: 
-  -  Replace declaration of `odrl:Policy` with `odrl:Set`
-  -  Fix licence name if licence name is set to `rdfs:label` to `dct:title` with 'en' lang string
-     - If the respective SPDX licence exists, copy the name from SPDX licence and remove `rdfs:label`,
-     - else, copy the value of `rdfs:label` and then remove `rdfs:label`.
-  -  If `cc:legalcode` is Literal copy to `dct:description` and remove it.
-  -  If policy has `rdfs:seeAlso` copy info to `cc:legalcode` and remove it
-  -  Add to policy provenance information
-     -  Add `prov:hadPrimarySource` the initial policy IRI
-     -  Add `prov:wasAttributedTo` the appropriated Actor
-        - Person :: Víctor Rodríguez Doncel
-        - Organization :: DALICC - Data Licenses Clearance Center
-  - Add `rdf:type odrl:Permission` if missing
-  - Add `rdf:type odrl:Prohibition` if missing
-  - Add `rdf:type odrl:Duty` if missing
-  - Fix `odrl:duty` property between policy and duty to `odrl:obligation`
-  - Add `edc:policy` wrapper
-
-To set which licence would be enriched with the respective SPDX licence set the `map_license_to_spdx` variable at the config file
-```
-map_license_to_spdx = {
-      'cc-by2.0': 'CC-BY-2.0',
-      'cc-by3.0': 'CC-BY-3.0',
-      'cc-by-nd3.0': 'CC-BY-ND-3.0',
-      'cc-by-nc-nd3.0': 'CC-BY-NC-ND-3.0',
-      'cc-by-nc-sa2.0': 'CC-BY-NC-SA-2.0',
-      'cc-by-nc-sa3.0': 'CC-BY-NC-SA-3.0',
-      'cc-by-sa3.0': 'CC-BY-SA-3.0',
-      }
-```
-
-### Use ::
-` python main.py -t`
-
-## Add standard licences into LDS-EDC
-In order to add the existing standard licences to your LDS-ECD, 
+# Add standard licences into LDS-EDC
+In order to add EDC policy representations from standard licences to your LDS-ECD, 
 1) copy `cp config.ini_sample config.ini`
 2) set the LDS-EDC address of the `config.ini` file at the variable `connector_address`. 
 ```
@@ -62,7 +9,7 @@ In order to add the existing standard licences to your LDS-ECD,
 connector_address = ADDRESS_TO_LDS_CONNECTOR
 ```
 Leave the rest of the information within the config file unchanged. This action will add the following standard 
-licences (found in folder LicencesAdded) within your LDS connector:  
+licences (found in folder LicencesToAdd) within your LDS connector:  
 * CC-BY-SA-4.0.json
 * UnderPSI
 * cc-by2.5se 
@@ -83,6 +30,6 @@ licences (found in folder LicencesAdded) within your LDS connector:
 * PublicDomain
 
 Leave the rest of the information without any changes.
-### Use ::
-`python main.py -a`
+## Use ::
+`python main.py`
 
--- main.py
+++ main.py
@@ -1,252 +1,7 @@
-import ast
-import getopt
-import json
 import configparser
-import sys
+import json
 
-import pyld
 import requests
-from rdflib import Graph, Literal, Namespace, BNode
-from rdflib.namespace import XSD, RDF, RDFS, DCTERMS, SKOS, FOAF, PROV
-
-dalicc_frame = {
-    "@context": {
-        "@vocab": "https://w3id.org/edc/v0.0.1/ns/",
-        "edc": "https://w3id.org/edc/v0.0.1/ns/",
-        "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
-        "dcat": "http://www.w3.org/ns/dcat#",
-        "xsd": "http://www.w3.org/2001/XMLSchema#",
-        "foaf": "http://xmlns.com/foaf/0.1/",
-        "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-        "adms": "http://www.w3.org/ns/adms#",
-        "dcmitype": "http://purl.org/dc/dcmitype/",
-        "skos": "http://www.w3.org/2004/02/skos/core#",
-        "dct": "http://purl.org/dc/terms/",
-        "odrl": "http://www.w3.org/ns/odrl/2/",
-        "owl": "http://www.w3.org/2002/07/owl#",
-        "cc": "http://creativecommons.org/ns#",
-        "dalicc": "https://dalicc.net/ns#",
-        "dalicclib": "https://dalicc.net/licenselibrary/",
-        "osl": "http://opensource.org/licenses/",
-        "prov": "http://www.w3.org/ns/prov#",
-        "spdx": "http://spdx.org/rdf/terms#"
-    },
-    "@type": "odrl:Set",
-    "odrl:permission": {"@type": "odrl:Permission", "odrl:duty": {"@type": "odrl:Duty", "@omitDefault": "true"}},
-    "odrl:prohibition": {"@type": "odrl:Prohibition", "odrl:remedy": {"@type": "odrl:Duty", "@omitDefault": "true"}},
-    "odrl:obligation": {"@type": "odrl:Duty", "odrl:consequence": {"@type": "odrl:Duty", "@omitDefault": "true"}},
-    "prov:wasAttributedTo": {"@omitDefault": "true"},
-}
-
-
-def retrieve_licences(licences_id: list, folder_init: str, source: str):
-    for id in licences_id:
-        print(f'Retrieving policy {id} from {source}')
-        if source == 'DALICC':
-            # Get licence from DALICC API
-            url = "https://api.dalicc.net/licenselibrary/license/{}?format=ttl&download=true"
-            response = requests.get(url.format(id))
-            initial_policy = response.text
-        elif source == 'Victor':
-            # Get licence from RDF License (Victor's service)
-            url = "https://raw.githubusercontent.com/oeg-upm/licensius/master/licensius-ws/src/main/resources/rdflicenses/{}.ttl"
-            response = requests.get(url.format(id))
-            initial_policy = response.text
-        elif source == 'SPDX':
-            # Get licence from SPDX
-            url = "https://raw.githubusercontent.com/spdx/license-list-data/main/rdfturtle/{}.ttl"
-            response = requests.get(url.format(id))
-            initial_policy = response.text
-
-        # Write initial license
-        with open(folder_init + '/{}_{}.ttl'.format(id, source), "w") as init_file:
-            init_file.write(initial_policy)
-
-
-def transform_licence_json_ld(licence_text: str, attribution_text: str, spdx_text: str) -> dict:
-    # Load licence to RDF graph and export to json-ld
-    g = Graph()
-    g.parse(data=licence_text)
-    g.parse(data=attribution_text)
-    if spdx_text is not None:
-        g.parse(data=spdx_text)
-
-    # Bind the FOAF namespace to a prefix for more readable output
-    odrl = Namespace('http://www.w3.org/ns/odrl/2/')
-    g.bind("odrl", odrl)
-    # Bind the CC namespace to a prefix for more readable output
-    cc = Namespace('http://creativecommons.org/ns#')
-    g.bind("cc", cc)
-    # Bind the SPDX namespace to a prefix for more readable output
-    spdx = Namespace('http://spdx.org/rdf/terms#')
-    g.bind("spdx", spdx)
-    # Bind the ADMS namespace to a prefix for more readable output
-    adms = Namespace('http://www.w3.org/ns/adms#')
-    g.bind("adms", adms)
-
-    # Replace odrl:Policy with odrl:Set
-    for policy in g.subjects(RDF.type, odrl.Policy):
-        g.add((policy, RDF.type, odrl.Set))
-        g.remove((policy, RDF.type, odrl.Policy))
-
-    # Victor - Fix name from SPDX licence
-    for policy in g.subjects(RDF.type, odrl.Set):
-        if (policy, RDFS.label, None) in g:
-            init_title = g.value(policy, RDFS.label)
-            g.remove((policy, RDFS.label, init_title))
-            # If respective spdx licence exists
-            if spdx_text is not None:
-                # remove rdfs:label, get spdx_name and  copy to dct:title with 'en' lang string
-                for spdx_policy in g.subjects(RDF.type, spdx.ListedLicense):
-                    title = g.value(spdx_policy, spdx.name)
-                    g.add((policy, DCTERMS.title, Literal(title, lang="en")))
-            else:
-                g.add((policy, DCTERMS.title, Literal(init_title, lang="en")))
-
-    # DALICC - Update dct:alternative with 'en' lang string
-    for policy in g.subjects(RDF.type, odrl.Set):
-        for alternative in g.objects(policy, DCTERMS.alternative):
-            g.add((policy, DCTERMS.alternative, Literal(alternative, lang="en")))
-            g.remove((policy, DCTERMS.alternative, alternative))
-
-    # DALICC - Update  dct:publisher with 'en' lang string
-    for policy in g.subjects(RDF.type, odrl.Set):
-        for publisher in g.objects(policy, DCTERMS.publisher):
-            g.add((policy, DCTERMS.publisher, Literal(publisher, lang="en")))
-            g.remove((policy, DCTERMS.publisher, publisher))
-
-    # DALICC - Remove spdx:licenseId if exists
-    for policy in g.subjects(RDF.type, odrl.Set):
-        for spdx_id in g.objects(policy, spdx.licenseId):
-            g.remove((policy, spdx.licenseId, spdx_id))
-
-    # Add dct:description the spdx:licenseText
-    for policy in g.subjects(RDF.type, odrl.Set):
-        # If respective spdx licence exists
-        if spdx_text is not None:
-            for spdx_policy in g.subjects(RDF.type, spdx.ListedLicense):
-                description = g.value(spdx_policy, spdx.licenseText)
-                g.add((policy, DCTERMS.description, Literal(description, lang="en")))
-        else:
-            description = g.value(policy, DCTERMS.title)
-            g.add((policy, DCTERMS.description, Literal(description, lang="en")))
-
-    # Victor - if policy cc:legalcode is Literal remove it
-    for policy in g.subjects(RDF.type, odrl.Set):
-        legal_code = g.value(policy, cc.legalcode)
-        if isinstance(legal_code, Literal):
-            g.remove((policy, cc.legalcode, legal_code))
-
-    # Victor - if policy has seeAlso copy info to cc:legalcode and remove it
-    for policy in g.subjects(RDF.type, odrl.Set):
-        legal_code = g.value(policy, RDFS.seeAlso)
-        if legal_code:
-            g.add((policy, cc.legalcode, legal_code))
-            g.remove((policy, RDFS.seeAlso, legal_code))
-
-    # Add to policy provenance information
-    # Add prov:hadPrimarySource the initial policy IRI
-    for policy in g.subjects(RDF.type, odrl.Set):
-        g.add((policy, PROV.hadPrimarySource, policy))
-    # Add prov:wasAttributedTo the appropriated
-    for policy in g.subjects(RDF.type, odrl.Set):
-        # Agent
-        attribution_to_agent = g.subjects(RDF.type, PROV.Agent)
-        for agent in attribution_to_agent:
-            g.add((policy, PROV.wasAttributedTo, agent))
-
-    # Add rdf:type Permission if missing
-    for permission in g.objects(None, odrl.permission):
-        if (permission, RDF.type, None) not in g:
-            g.add((permission, RDF.type, odrl.Permission))
-    # Add rdf:type Prohibition if missing
-    for prohibition in g.objects(None, odrl.prohibition):
-        if (prohibition, RDF.type, None) not in g:
-            g.add((prohibition, RDF.type, odrl.Prohibition))
-    # Add rdf:type Duty if missing
-    for duty in g.objects(None, odrl.obligation):
-        if (duty, RDF.type, None) not in g:
-            g.add((duty, RDF.type, odrl.Duty))
-    for duty in g.objects(None, odrl.duty):
-        if (duty, RDF.type, None) not in g:
-            g.add((duty, RDF.type, odrl.Duty))
-    for duty in g.objects(None, odrl.remedy):
-        if (duty, RDF.type, None) not in g:
-            g.add((duty, RDF.type, odrl.Duty))
-    for duty in g.objects(None, odrl.consequence):
-        if (duty, RDF.type, None) not in g:
-            g.add((duty, RDF.type, odrl.Duty))
-
-    # Add SPDX id as adms:identifier
-    # If respective spdx licence exists
-    for policy in g.subjects(RDF.type, odrl.Set):
-        if spdx_text is not None:
-            for spdx_policy in g.subjects(RDF.type, spdx.ListedLicense):
-                licenseId = g.value(spdx_policy, spdx.licenseId)
-                adms_id = BNode()
-                g.add((adms_id, RDF.type, adms.Identifier))
-                g.add((adms_id, SKOS.notation, Literal(licenseId, datatype=XSD.string)))
-                g.add((adms_id, adms.schemaAgency, Literal("SPDX", lang="en")))
-                g.add((policy, adms.identifier, adms_id))
-
-    # Serialize to json-ld
-    licence_jsonld_serialized = g.serialize(format='json-ld')
-
-    # Transform json-ld to framed json-ld (tree representation)
-    data = json.loads(licence_jsonld_serialized)
-    licence_framed_json_ld = pyld.jsonld.frame(data, dalicc_frame)
-
-    # Fix odrl:duty property between policy and duty to odrl:obligation
-    if type(licence_framed_json_ld.get('odrl:duty')) == list:
-        for d in licence_framed_json_ld.get('odrl:duty'):
-            licence_framed_json_ld['odrl:obligation'] = d
-        del licence_framed_json_ld['odrl:duty']
-    elif type(licence_framed_json_ld.get('odrl:duty')) == dict:
-        licence_framed_json_ld['odrl:obligation'] = licence_framed_json_ld['odrl:duty']
-        del licence_framed_json_ld['odrl:duty']
-
-    # Add edc:policy wrapper
-    edc_licence_framed_json_ld = dict()
-    context = licence_framed_json_ld['@context']
-    context['edc'] = 'https://w3id.org/edc/v0.0.1/ns/'
-    del licence_framed_json_ld['@context']
-    edc_licence_framed_json_ld['@context'] = context
-    edc_licence_framed_json_ld['policy'] = licence_framed_json_ld
-    return edc_licence_framed_json_ld
-
-
-def transform_licences(licences_id: list, folder_init: str, folder_added: str, source: str, prov_attribution_file: str,
-                       spdx_licences_id: dict):
-    # Read ttl file with provenance attribution
-    f = open('./{}'.format(prov_attribution_file), "r")
-    prov_attribution_text = f.read()
-    f.close()
-
-    for i, id in enumerate(licences_id):
-        print(f'Transforming policy {id}')
-        # Read ttl file with policy
-        f = open(folder_init + '/{}_{}.ttl'.format(id, source), "r")
-        initial_policy = f.read()
-        f.close()
-
-        spdx_policy = None
-        spdx_id = None
-        if spdx_licences_id is not None and id in spdx_licences_id:
-            # Read ttl file with respective SPDX policy
-            spdx_id = spdx_licences_id[id]
-            print(f'Open spdc file {spdx_id} for policy {id}')
-            f = open(folder_init + '/{}_SPDX.ttl'.format(spdx_id), "r")
-            spdx_policy = f.read()
-            f.close()
-
-        transformed_policy = transform_licence_json_ld(initial_policy, prov_attribution_text, spdx_policy)
-        # Write transformed license
-        if spdx_id:
-            with open(folder_added + '/{}.json'.format(spdx_id), "w") as tranformed_file:
-                tranformed_file.write(json.dumps(transformed_policy, indent=4))
-        else:
-            with open(folder_added + '/{}.json'.format(id), "w") as tranformed_file:
-                tranformed_file.write(json.dumps(transformed_policy, indent=4))
 
 
 def create_policy_on_lds_proxy(create_policy_url: str, suggest_licence_endpoint: str,
@@ -297,89 +52,28 @@ if __name__ == "__main__":
     lds_proxy_create_policy_endpoint = config['DEFAULT']['create_policy_endpoint']
     lds_proxy_suggest_policy_endpoint = config['DEFAULT']['suggest_licence_endpoint']
     notes = dict()
-    dalicc_licences_id = ast.literal_eval(config['DEFAULT']['dalicc_licences_id'])
-    dalicc_attribution_file = config['DEFAULT']['dalicc_attribution_file']
 
-    rdfLicense_licences_id = ast.literal_eval(config['DEFAULT']['rdfLicense_licences_id'])
-    rdfLicense_attribution_file = config['DEFAULT']['rdfLicense_attribution_file']
+    # Retrieve licences ids to add to connectors
+    print('Retrieve licences ids to add to connectors')
+    licences_id = []
+    from os import listdir
+    from os.path import isfile, join
 
-    spdx_id_rdfLicense = ast.literal_eval(config['DEFAULT']['spdx_id_rdfLicense'])
-    map_license_to_spdx = ast.literal_eval(config['DEFAULT']['map_license_to_spdx'])
+    licences_id = [f for f in listdir(config['DEFAULT']['folder_licences_added'])
+                   if isfile(join(config['DEFAULT']['folder_licences_added'], f))]
+    print(licences_id)
+    # Add policies to connectors
+    print('Adding EDC policies to connectors')
 
-    # Remove 1st argument from the
-    # list of command line arguments
-    argumentList = sys.argv[1:]
+    for section in config.sections():
+        connector_address = config[section]['connector_address']
+        connector_policy_endpoint = lds_proxy_create_policy_endpoint.format(connector_address)
+        connector_suggest_policy_endpoint = lds_proxy_suggest_policy_endpoint.format(connector_address)
+        notes[connector_address] = create_policy_on_lds_proxy(connector_policy_endpoint,
+                                                              connector_suggest_policy_endpoint,
+                                                              licences_id,
+                                                              config['DEFAULT']['folder_licences_added']
+                                                              )
 
-    # Options
-    options = "rta"
-
-    # Long options
-    long_options = ["Retrieve", "Transform", "Add"]
-
-    try:
-        # Parsing argument
-        arguments, values = getopt.getopt(argumentList, options, long_options)
-
-        # checking each argument
-        for currentArgument, currentValue in arguments:
-
-            if currentArgument in ("-r", "--Retrieve"):
-                # Retrieve initial policies and save to file
-                print('Retrieving standard lincences as ODRL representation')
-                retrieve_licences(dalicc_licences_id,
-                                  config['DEFAULT']['folder_licences_init'],
-                                  'DALICC')
-                retrieve_licences(rdfLicense_licences_id,
-                                  config['DEFAULT']['folder_licences_init'],
-                                  'Victor')
-                retrieve_licences(spdx_id_rdfLicense,
-                                  config['DEFAULT']['folder_licences_init'],
-                                  'SPDX')
-            elif currentArgument in ("-t", "--Transform"):
-                # Transform from ttl to edc policy
-                print('Transforming to EDC policy representation')
-                transform_licences(dalicc_licences_id,
-                                   config['DEFAULT']['folder_licences_init'],
-                                   config['DEFAULT']['folder_licences_added'],
-                                   'DALICC',
-                                   dalicc_attribution_file,
-                                   map_license_to_spdx
-                                   )
-                transform_licences(rdfLicense_licences_id,
-                                   config['DEFAULT']['folder_licences_init'],
-                                   config['DEFAULT']['folder_licences_added'],
-                                   'Victor',
-                                   rdfLicense_attribution_file,
-                                   map_license_to_spdx
-                                   )
-
-            elif currentArgument in ("-a", "--Add"):
-                # Retrieve licences  ids to add to connectors
-                print('Retrieve licences  ids to add to connectors')
-                licences_id = []
-                from os import listdir
-                from os.path import isfile, join
-
-                licences_id = [f for f in listdir(config['DEFAULT']['folder_licences_added'])
-                               if isfile(join(config['DEFAULT']['folder_licences_added'], f)) and
-                               f != 'OdcPublicDomainDedicationAndLicence.json' and
-                               f != 'notes.json']
-                print(licences_id)
-                # Add policies to connectors
-                print('Adding EDC policies to connectors')
-
-                for section in config.sections():
-                    connector_address = config[section]['connector_address']
-                    connector_policy_endpoint = lds_proxy_create_policy_endpoint.format(connector_address)
-                    connector_suggest_policy_endpoint = lds_proxy_suggest_policy_endpoint.format(connector_address)
-                    notes[connector_address] = create_policy_on_lds_proxy(connector_policy_endpoint,
-                                                                          connector_suggest_policy_endpoint,
-                                                                          licences_id,
-                                                                          config['DEFAULT']['folder_licences_added']
-                                                                          )
-
-                with open(config['DEFAULT']['folder_licences_added'] + '/notes.json', "w") as notefile:
-                    notefile.write(json.dumps(notes, indent=4))
-    except getopt.error as err:
-        # output error, and return with an error code
-        print(str(err))
+    with open('notes.json', "w") as notefile:
+        notefile.write(json.dumps(notes, indent=4))
