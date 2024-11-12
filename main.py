647d81baacd0e845e8fa97caf298bc991d5d05cd
Katerina Gkirtzou <katerina.gkirtzou@gmail.com>
2024-11-12 18:19:01 +0200

    Add authorization handling to licence import

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import configparser
 import json
-
+from http import HTTPStatus
 import requests
 
 
 def create_policy_on_lds_proxy(create_policy_url: str, suggest_licence_endpoint: str,
+                               connector_keycloak_endpoint: str,
+                               get_token_payload: dict,
                                licences_id: list, folder_added: str):
+
+    response_auth = requests.post(connector_keycloak_endpoint, data=get_token_payload)
+    if response_auth.status_code != requests.codes.ok:
+        print(f'Error occured during getting token: {response_auth}')
+        return
+    # Get access_token and refresh_token
+    access_token = response_auth.json()['access_token']
+    refresh_token = response_auth.json()['refresh_token']
     notes = dict()
     for id in licences_id:
         print(f'Working on policy {id}')
@@ -19,14 +29,19 @@ def create_policy_on_lds_proxy(create_policy_url: str, suggest_licence_endpoint:
         # Search if policy exists
         print(f'Checking if policy {id} already exists')
         title = edc_policy['policy']['dct:title']['@value']
-        payload = {
+        license_params = {
             'exact': 'true',
             'title': title
         }
-        response_licence = requests.get(suggest_licence_endpoint, params=payload)
+        license_headers = {
+            'x-api-key': 'password',
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {access_token}'
+        }
+        response_licence_search = requests.get(suggest_licence_endpoint, headers=license_headers, params=license_params)
         found = False
-        if response_licence.status_code == requests.codes.ok:
-            for p_lic in response_licence.json()['data']:
+        if response_licence_search.status_code == requests.codes.ok:
+            for p_lic in response_licence_search.json()['data']:
                 if p_lic['title'] == title:
                     print(f'Found at {p_lic["id"]}')
                     found = True
@@ -35,13 +50,13 @@ def create_policy_on_lds_proxy(create_policy_url: str, suggest_licence_endpoint:
         if not found:
             # Create policy on lds-edc connector
             print(f'Creating policy {id}')
-            response = requests.post(create_policy_url, json=edc_policy)
-            if response.status_code == requests.codes.ok:
-                notes[id] = json.loads(response.text)['data']['@id']
+            response_licence_create = requests.post(create_policy_url, headers=license_headers, json=edc_policy)
+            if response_licence_create.status_code == requests.codes.ok:
+                notes[id] = json.loads(response_licence_create.text)['data']['@id']
                 print(f'Policy created successfully with {id}')
             else:
-                notes[id] = response.text
-                print(f'Policy creation failed. Error: {response.text}')
+                notes[id] = response_licence_create.text
+                print(f'Policy creation failed. Error: {response_licence_create.text}')
     return notes
 
 
@@ -51,6 +66,7 @@ if __name__ == "__main__":
     config.read('config.ini')
     lds_proxy_create_policy_endpoint = config['DEFAULT']['create_policy_endpoint']
     lds_proxy_suggest_policy_endpoint = config['DEFAULT']['suggest_licence_endpoint']
+    lds_keycloak_endpoint = config['DEFAULT']['get_token_endpoint']
     notes = dict()
 
     # Retrieve licences ids to add to connectors
@@ -67,10 +83,21 @@ if __name__ == "__main__":
 
     for section in config.sections():
         connector_address = config[section]['connector_address']
+        keycloak_address = config[section]['keycloak_address']
+        get_token_payload = dict()
+        get_token_payload['username'] = config[section]['username']
+        get_token_payload['password'] = config[section]['password']
+        get_token_payload['client_id'] = config[section]['client_id']
+        get_token_payload['client_secret'] = config[section]['client_secret']
+        get_token_payload['grant_type'] = 'password'
+
         connector_policy_endpoint = lds_proxy_create_policy_endpoint.format(connector_address)
         connector_suggest_policy_endpoint = lds_proxy_suggest_policy_endpoint.format(connector_address)
+        connector_keycloak_endpoint = lds_keycloak_endpoint.format(keycloak_address)
         notes[connector_address] = create_policy_on_lds_proxy(connector_policy_endpoint,
                                                               connector_suggest_policy_endpoint,
+                                                              connector_keycloak_endpoint,
+                                                              get_token_payload,
                                                               licences_id,
                                                               config['DEFAULT']['folder_licences_added']
                                                               )
